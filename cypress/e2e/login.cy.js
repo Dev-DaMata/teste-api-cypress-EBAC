caa73b59624d7ad48a7a866a33121fc601d70cc0
Dev-DaMata <gcordeiro773@gmail.com>
2025-07-30 14:41:13 -0300

    Aula1: Preparacao do ambiente

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,21 @@
+/// <reference types="cypress"/>
+
+describe('Teste de API - Login', () => {
+  it('Deve realizar login com sucesso', () => {
+    cy.request({
+      method: 'POST',
+      url: 'login',
+      body: {
+        "email": "fulano@qa.com",
+        "password": "teste"
+      }
+    })/*.should((response) =>{ //Esse é o jeito do curso 
+      expect(response.body.message).eq("Login realizado com sucesso"),
+      expect(response.status).eq(200)
+    })*/
+    .then(res => { //Meu jeito
+      expect(res.body.message).eq("Login realizado com sucesso"),
+      expect(res.status).eq(200)
+    })
+  })
+})
